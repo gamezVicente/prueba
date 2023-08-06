316d939c2b02eabba8d0b09cdbdfe8385fa36d11
gamezVicente <gamezvicente44@gmail.com>
2023-08-06 02:28:09 -0600

    change

--- portfolio.py
+++ portfolio.py
@@ -6,5 +6,5 @@ bp = Blueprint('portfolio', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('portfolio/index.html')
+    return render_template('templates/portfolio/index.html')
 
