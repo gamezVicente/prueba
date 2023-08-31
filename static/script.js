889326bf6f26298135ee43a9a288611c28274693
vicenteGamezz <ceinnova07721@innovaccion.mx>
2023-08-31 15:50:23 -0600

    commit

--- static/script.js
+++ static/script.js
@@ -4,8 +4,7 @@ const projectHideBtn = document.querySelector(".project-hide-btn");
 
 projects.forEach(project => {
     project.addEventListener('mouseenter', () => {
-        project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20
-        }px`;
+        project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20}px`;
     });
 
     project.addEventListener('mouseleave', () => {
@@ -20,7 +19,7 @@ projects.forEach(project => {
         const bigImg = document.createElement('img');
         bigImg.className = "project-img";
         const imgPath = project.firstElementChild.getAttribute("src").split(".")[4];
-        bigImg.setAttribute("src", `${imgPath}-big.jpg`)
+        bigImg.setAttribute("src", `${imgPath}-big.jpg`);
         bigImgWrapper.appendChild(bigImg);
         document.body.style.overflowY = "hidden";
         
